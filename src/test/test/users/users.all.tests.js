64901a91cce7fc0cc5346491507ae9750e76a8e3
Cristian Herghelegiu <herr.cristi@gmail.com>
2023-07-29 19:49:41 +0300

    Add base controller

--- src/test/test/users/users.all.tests.js
+++ src/test/test/users/users.all.tests.js
@@ -5,12 +5,7 @@
  */
 
 // controller tests
-require('./users.controller.getAll.test.js');
-require('./users.controller.getOne.test.js');
-require('./users.controller.post.test.js');
-require('./users.controller.delete.test.js');
-require('./users.controller.put.test.js');
-require('./users.controller.patch.test.js');
+require('./users.controller.all.test.js');
 
 // rest tests
 require('./users.rest.test.js');
