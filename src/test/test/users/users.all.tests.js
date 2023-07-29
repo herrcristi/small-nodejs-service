aa4a86ceb7e79f0443d63a0a99e59af77914ad46
Cristian Herghelegiu <56673580+herrcristi@users.noreply.github.com>
2023-07-29 20:10:12 +0300

    Refactor services tests (#9)

--- src/test/test/users/users.all.tests.js
+++ src/test/test/users/users.all.tests.js
@@ -11,14 +11,7 @@ require('./users.controller.all.test.js');
 require('./users.rest.test.js');
 
 // service tests
-require('./users.service.getAllForReq.test.js');
-require('./users.service.getAll.test.js');
-require('./users.service.getAllCount.test.js');
-require('./users.service.getAllByIDs.test.js');
-require('./users.service.getOne.test.js');
-require('./users.service.post.test.js');
-require('./users.service.delete.test.js');
-require('./users.service.put.test.js');
-require('./users.service.patch.test.js');
+require('./users.service.all.test.js');
 
+// schema tests
 require('./users.service.schema.test.js');
