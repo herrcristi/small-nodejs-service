5f375387d2aa74c159e13fd31c3728a6e913e3f9
Cristian Herghelegiu <herr.cristi@gmail.com>
2024-04-11 00:00:52 +0300

    Local login

--- src/test/test/groups/groups.rest.test.js
+++ src/test/test/groups/groups.rest.test.js
@@ -72,7 +72,7 @@ describe('Groups Rest', function () {
     });
 
     // call
-    let res = await GroupsRest.getOne('id1', _ctx);
+    let res = await GroupsRest.getOne('id1', {}, _ctx);
     console.log(`\nTest returned: ${JSON.stringify(res, null, 2)}\n`);
 
     // check
