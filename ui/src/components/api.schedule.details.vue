352afdbdb08bf1127d09162765bca6f351496ac3
Cristian Herghelegiu <herr.cristi@gmail.com>
2025-11-25 21:39:35 +0200

    Allow more customization for each component

--- ui/src/components/api.schedule.details.vue
+++ ui/src/components/api.schedule.details.vue
@@ -1,152 +1,13 @@
 <template>
-  <v-card v-if="read || write">
-    <!-- 
-        for details 
-    -->
-    <ApiDetails
-      ref="detailsComponent"
-      :itemID="props.itemID"
-      :read="read"
-      :apiFn="{
-        get: Api.getSchedule,
-      }"
-      @loading="loading = $event"
-      @nodatatext="nodatatext = $event"
-      @item="onItemDetails($event)"
-    ></ApiDetails>
-
-    <!-- 
+  <!-- 
         dialog title
     -->
-    <v-card-title class="d-flex justify-space-between">
-      <div>{{ itemDetails.name || t('details') }}</div>
-      <v-btn icon small @click="closeDialog"><v-icon>mdi-close</v-icon></v-btn>
-    </v-card-title>
+  <v-card-title class="d-flex justify-space-between">
+    <div>{{ itemName || t('details') }}</div>
+    <v-btn icon small @click="closeDialog"><v-icon>mdi-close</v-icon></v-btn>
+  </v-card-title>
 
-    <v-card-text>
-      <!-- 
-          field classes 
-      -->
-      <ApiRefClasses
-        ref="fieldDetailsClassesComponent"
-        title="classes"
-        :titleAdd="itemDetails.name"
-        :items="fieldClasses"
-        :apiFn="{}"
-        :read="read"
-        :write="false"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefClasses>
-    </v-card-text>
-
-    <v-card-text>
-      <!-- 
-          field professors 
-      -->
-      <ApiRefProfessors
-        ref="fieldDetailsProfessorsComponent"
-        title="professors"
-        :titleAdd="itemDetails.name"
-        :items="fieldProfessors"
-        :apiFn="{
-          getAllParams: { 'classes.id': itemDetails.class?.id },
-          updateField: updateFieldProfessors,
-          deleteField: deleteFieldProfessor,
-        }"
-        :read="read"
-        :write="write"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefProfessors>
-    </v-card-text>
-
-    <v-card-text>
-      <!-- 
-          field inner schedules
-      -->
-      <ApiRefInnerSchedules
-        ref="fieldDetailsInnerSchedulesComponent"
-        title="schedules"
-        :titleAdd="itemDetails.name"
-        :items="fieldInnerSchedules"
-        :apiFn="{
-          addField: addFieldInnerSchedule,
-          updateField: updateFieldInnerSchedules,
-          deleteField: deleteFieldInnerSchedule,
-        }"
-        :read="read"
-        :write="write"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefInnerSchedules>
-    </v-card-text>
-
-    <v-card-text>
-      <!-- 
-          field groups 
-      -->
-      <ApiRefGroups
-        ref="fieldDetailsGroupsComponent"
-        title="groups"
-        :titleAdd="itemDetails.name"
-        :items="fieldGroups"
-        :apiFn="{
-          // getAllParams: { 'classes.id': itemDetails.class?.id },
-          updateField: updateFieldGroups,
-          deleteField: deleteFieldGroup,
-        }"
-        :read="read"
-        :write="write"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefGroups>
-    </v-card-text>
-
-    <v-card-text>
-      <!-- 
-          field students from groups
-      -->
-      <ApiRefStudents
-        ref="fieldDetailsGroupsStudentsComponent"
-        title="schedules.groups.students"
-        :titleAdd="itemDetails.name"
-        :items="fieldGroupsStudents"
-        :apiFn="{}"
-        :read="read"
-        :write="false"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefStudents>
-    </v-card-text>
-
-    <v-card-text>
-      <!-- 
-          field students extra
-      -->
-      <ApiRefStudents
-        ref="fieldDetailsExtraStudentsComponent"
-        title="schedules.extra.students"
-        :titleAdd="itemDetails.name"
-        :items="fieldExtraStudents"
-        :apiFn="{
-          getAllParams: { 'classes.id': itemDetails.class?.id },
-          updateField: updateFieldExtraStudents,
-          deleteField: deleteFieldExtraStudent,
-        }"
-        :read="read"
-        :write="write"
-        :loading="loading"
-        :nodatatext="nodatatext"
-      >
-      </ApiRefStudents>
-    </v-card-text>
-  </v-card>
+  <ApiScheduleMoreInfo ref="moreInfoComponent" :itemID="itemID" type="v-table" />
 
   <!-- add extra space -->
   <v-card>
@@ -159,12 +20,7 @@
 
 <script setup>
 import { ref, reactive, computed, watch } from 'vue';
-import ApiDetails from './api.base.details.vue';
-import ApiRefClasses from './api.references.classes.vue';
-import ApiRefGroups from './api.references.groups.vue';
-import ApiRefStudents from './api.references.students.vue';
-import ApiRefProfessors from './api.references.professors.vue';
-import ApiRefInnerSchedules from './api.references.innerschedules.vue';
+import ApiScheduleMoreInfo from './api.schedule.moreinfo.vue';
 import Api from '../api/api.js';
 import { useAppStore } from '../stores/stores.js';
 import { useI18n } from 'vue-i18n';
@@ -175,37 +31,10 @@ const { t } = useI18n();
  */
 const props = defineProps({
   itemID: { type: String, default: null },
+  itemName: { type: String, default: null },
 });
 
-const detailsComponent = ref();
-const itemDetails = ref({});
-const loading = ref(false);
-const nodatatext = ref('');
-
-const app = useAppStore();
-const read = app?.rolesPermissions?.schedules?.read || 0;
-const write = app?.rolesPermissions?.schedules?.write || 0;
-
-/**
- * fields
- */
-const fieldDetailsClassesComponent = ref();
-const fieldClasses = ref([]);
-
-const fieldDetailsProfessorsComponent = ref();
-const fieldProfessors = ref([]);
-
-const fieldDetailsInnerSchedulesComponent = ref();
-const fieldInnerSchedules = ref([]);
-
-const fieldDetailsGroupsComponent = ref();
-const fieldGroups = ref([]);
-
-const fieldDetailsGroupsStudentsComponent = ref();
-const fieldGroupsStudents = ref([]);
-
-const fieldDetailsExtraStudentsComponent = ref();
-const fieldExtraStudents = ref([]);
+const moreInfoComponent = ref();
 
 /**
  * emit
@@ -213,133 +42,11 @@ const fieldExtraStudents = ref([]);
 const emit = defineEmits(['close']);
 
 /**
- * ApiDetails events after get details
- */
-async function onItemDetails(data) {
-  Object.keys(itemDetails.value).forEach((k) => delete itemDetails[k]);
-  Object.assign(itemDetails.value, data);
-
-  fieldClasses.value = [itemDetails.value.class];
-  fieldProfessors.value = itemDetails.value.professors || [];
-  fieldInnerSchedules.value = itemDetails.value.schedules || [];
-
-  fieldGroups.value = itemDetails.value.groups || [];
-  fieldGroupsStudents.value = getGroupsStudents(fieldGroups.value);
-  fieldExtraStudents.value = itemDetails.value.students || [];
-}
-
-function getGroupsStudents(groups) {
-  let students = [];
-  groups.forEach((group) => students.push(...group.students));
-
-  // eliminate duplicates
-  let studentsIDs = new Set(students.map((item) => item.id));
-  let uniqueStudents = [];
-  for (const student of students) {
-    if (studentsIDs.has(student.id)) {
-      uniqueStudents.push(student);
-      studentsIDs.delete(student.id);
-    }
-  }
-
-  return uniqueStudents;
-}
-
-/**
  * close details dialog
  */
 function closeDialog() {
-  fieldDetailsClassesComponent.value.clear();
-  fieldDetailsGroupsComponent.value.clear();
-  fieldDetailsProfessorsComponent.value.clear();
-  fieldDetailsGroupsStudentsComponent.value.clear();
-  fieldDetailsExtraStudentsComponent.value.clear();
+  moreInfoComponent.value.clear();
   emit('close');
-}
-
-/**
- * ApiFieldDetails calling inner schedules addField / deleteField / updateField
- */
-async function addFieldInnerSchedule(data) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleInnerSchedules(props.itemID, [data], null);
-
-  // refresh
-  await detailsComponent.value.refresh();
-}
-
-async function deleteFieldInnerSchedule(innerScheduleID) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleInnerSchedules(props.itemID, [], [innerScheduleID]);
-
-  // no need for server call
-  fieldInnerSchedules.value = fieldInnerSchedules.value.filter((item) => item.id !== innerScheduleID);
-}
-
-async function updateFieldInnerSchedules(innerScheduleID, payload) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleInnerSchedules(props.itemID, [payload], [innerScheduleID]);
-
-  // refresh
-  await detailsComponent.value.refresh();
-}
-
-/**
- * * ApiFieldDetails calling groups: deleteField / updateField
- */
-async function deleteFieldGroup(groupID) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleGroups(props.itemID, [], [groupID]);
-
-  // no need for server call
-  fieldGroups.value = fieldGroups.value.filter((item) => item.id !== groupID);
-  fieldGroupsStudents.value = getGroupsStudents(fieldGroups.value);
-}
-
-async function updateFieldGroups(newIDs, removeIDs) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleGroups(props.itemID, newIDs, removeIDs);
-
-  // refresh
-  await detailsComponent.value.refresh();
-}
-
-/**
- * ApiFieldDetails calling professors: deleteField / updateField
- */
-async function deleteFieldProfessor(professorID) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleProfessors(props.itemID, [], [professorID]);
-
-  // no need for server call
-  fieldProfessors.value = fieldProfessors.value.filter((item) => item.id !== professorID);
-}
-
-async function updateFieldProfessors(newIDs, removeIDs) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleProfessors(props.itemID, newIDs, removeIDs);
-
-  // refresh
-  await detailsComponent.value.refresh();
-}
-
-/**
- * ApiFieldDetails calling extra students: deleteField / updateField
- */
-async function deleteFieldExtraStudent(studentID) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleStudents(props.itemID, [], [studentID]);
-
-  // no need for server call
-  fieldExtraStudents.value = fieldExtraStudents.value.filter((item) => item.id !== studentID);
-}
-
-async function updateFieldExtraStudents(newIDs, removeIDs) {
-  // if fail will throw error and be catch in ApiFieldDetails
-  await Api.updateScheduleStudents(props.itemID, newIDs, removeIDs);
-
-  // refresh
-  await detailsComponent.value.refresh();
 }
 </script>
 
--- ui/src/components/api.schools.vue
+++ ui/src/components/api.schools.vue
@@ -1,36 +1,262 @@
 <template>
-  <ApiPage
-    title="schools"
-    :fields="['name', 'status', 'description']"
-    :projectionFields="['name', 'status', 'description']"
-    :sortFields="['name', 'status']"
-    :filterFields="['name', '_lang_en.status', 'description']"
-    :apiFn="{
-      getAll: Api.getSchools,
-      create: Api.createSchool,
-      update: Api.updateSchool,
-      delete: Api.deleteSchool,
-    }"
-    :read="read"
-    :write="write"
-  ></ApiPage>
+  <v-card v-if="read || write">
+    <!-- 
+          table
+    -->
+    <ApiTableServer
+      ref="tableServer"
+      title="schools"
+      :fields="apiFields.fields"
+      :projectionFields="apiFields.projectionFields"
+      :sortFields="apiFields.sortFields"
+      :filterFields="apiFields.filterFields"
+      :apiFn="apiFn"
+      :read="read"
+      :write="write"
+      :details="details"
+      :expand="expand"
+      @addItem="openAdd($event)"
+      @editItem="openEdit($event)"
+      @detailsItem="openDetails($event)"
+    >
+    </ApiTableServer>
+
+    <!-- 
+          edit dialog
+    -->
+    <v-card v-if="write">
+      <v-dialog v-model="dialog" max-width="800px" v-if="write">
+        <v-card>
+          <v-card-title>
+            {{ editing ? t('edit') : t('add') }}
+          </v-card-title>
+
+          <v-card-text>
+            <v-form ref="editForm" v-model="formValid">
+              <slot name="edit.name" :itemData="itemData" :fieldsSet="fieldsSet">
+                <v-text-field
+                  v-if="fieldsSet.has('name')"
+                  v-model="itemData.name"
+                  :label="t('name')"
+                  :rules="[nameRule]"
+                  required
+                />
+              </slot>
+
+              <slot name="edit.status" :itemData="itemData" :fieldsSet="fieldsSet">
+                <v-select
+                  v-if="fieldsSet.has('status')"
+                  v-model="itemData.status"
+                  :items="statusItems"
+                  item-title="title"
+                  item-value="value"
+                  :label="t('status')"
+                  :rules="[statusRule]"
+                  required
+                />
+              </slot>
+
+              <slot name="edit.description" :itemData="itemData" :fieldsSet="fieldsSet">
+                <v-text-field
+                  v-if="fieldsSet.has('description')"
+                  v-model="itemData.description"
+                  :label="t('description')"
+                  required
+                />
+              </slot>
+            </v-form>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text @click="closeDialog">{{ t('cancel') }}</v-btn>
+            <v-btn color="primary" :disabled="!formValid" @click="handleSubmit">{{ t('save') }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <!-- 
+        snackbar for notifications
+      -->
+      <v-snackbar v-model="snackbar" :color="snackbarColor" timeout="4000">{{ snackbarText }}</v-snackbar>
+    </v-card>
+  </v-card>
 </template>
 
 <script setup>
 import { ref, reactive, computed } from 'vue';
 import Api from '../api/api.js';
 import { useAppStore } from '../stores/stores.js';
-import ApiPage from './api.base.page.vue';
+import ApiTableServer from './api.base.table.server.vue';
 import { useI18n } from 'vue-i18n';
+import ComponentsUtils from './components.utils.js';
 const { t } = useI18n();
 
 /**
  * props
  */
+const tableServer = ref();
+const details = null; // true for details column
+const expand = null; // true for expand column
 
 const app = useAppStore();
-const read = app?.rolesPermissions?.groups?.read;
-const write = app?.rolesPermissions?.groups?.write;
+const read = app?.rolesPermissions?.schools?.read;
+const write = app?.rolesPermissions?.schools?.write;
+
+/**
+ * fields
+ */
+const apiFields = {
+  fields: ['name', 'status', 'description'],
+  projectionFields: ['name', 'status', 'description'],
+  sortFields: ['name', 'status'],
+  filterFields: ['name', '_lang_en.status', 'description'],
+  addFields: ['name', 'status', 'description'],
+  editFields: ['name', 'status', 'description'],
+};
+
+const fieldsSet = ref(new Set([]));
+
+const apiFn = {
+  getAll: Api.getSchools,
+  create: Api.createSchool,
+  update: Api.updateSchool,
+  delete: Api.deleteSchool,
+};
+
+/**
+ * edit
+ */
+const itemData = reactive({});
+const editing = ref(false);
+const editingItemID = ref(null);
+const dialog = ref(false);
+const editForm = ref(null);
+const formValid = ref(false);
+
+const snackbar = ref(false);
+const snackbarText = ref('');
+const snackbarColor = ref('');
+
+const statusItems = computed(() =>
+  ComponentsUtils.Edit.statusItems(t, {
+    skipPending: editing.value,
+  })
+);
+
+/**
+ * rules
+ */
+const nameRule = (v) => ComponentsUtils.Edit.Rules.name(v, t);
+const statusRule = (v) => ComponentsUtils.Edit.Rules.status(v, t);
+
+/**
+ * ApiTableServer event open details
+ */
+function openDetails(item) {
+  // noop
+}
+
+/**
+ * handle submit
+ */
+async function handleSubmit() {
+  if (editForm.value && typeof editForm.value.validate === 'function') {
+    const ok = await editForm.value.validate();
+    if (!ok) {
+      return;
+    }
+  }
+
+  // trim
+  ComponentsUtils.Edit.trimFields(itemData, ['name', 'description']);
+
+  // payload
+  const payload = { ...itemData };
+  const snackbarO = { ref: snackbar, text: snackbarText, color: snackbarColor };
+
+  // add/update
+  let ok = false;
+  if (editing.value) {
+    ok = await ComponentsUtils.Edit.update(apiFn, editingItemID.value, payload, snackbarO, t);
+  } else {
+    ok = await ComponentsUtils.Edit.add(apiFn, payload, snackbarO, t);
+  }
+
+  if (ok) {
+    dialog.value = false;
+    resetForm();
+
+    // refresh
+    await tableServer.value.refresh();
+  }
+}
+
+/**
+ * open add dialog
+ */
+function openAdd() {
+  if (!write) {
+    return;
+  }
+
+  resetForm();
+  fieldsSet.value = new Set(apiFields.addFields);
+
+  // defaults for new
+  if (fieldsSet.value.has('status')) {
+    itemData.status = 'active';
+  }
+
+  editing.value = false;
+  dialog.value = true;
+}
+
+/**
+ * open edit dialog
+ */
+function openEdit(item) {
+  if (!write) {
+    return;
+  }
+
+  resetForm();
+  fieldsSet.value = new Set(apiFields.editFields);
+
+  // set
+  if (fieldsSet.value.has('name')) {
+    itemData.name = item.name || '';
+  }
+  if (fieldsSet.value.has('status')) {
+    itemData.status = item.status || 'active';
+  }
+  if (fieldsSet.value.has('description')) {
+    itemData.description = item.description || '';
+  }
+
+  editing.value = true;
+  editingItemID.value = item.id;
+  dialog.value = true;
+}
+
+/**
+ * close dialog
+ */
+function closeDialog() {
+  dialog.value = false;
+  resetForm();
+}
+
+/**
+ * reset form
+ */
+function resetForm() {
+  Object.keys(itemData).forEach((k) => delete itemData[k]);
+  fieldsSet.value = new Set([]);
+
+  editing.value = false;
+  editingItemID.value = null;
+}
 
 /**
  * mounted
